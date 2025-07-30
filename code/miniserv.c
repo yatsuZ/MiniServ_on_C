2a8f16558e1c72622ff4444a5cff8c8f698e2cdf
yassine <yassine.zaoui28062003@gmail.com>
2025-07-30 15:41:32 +0200

    message de bien venue fais

--- code/miniserv.c
+++ code/miniserv.c
@@ -7,6 +7,8 @@
 #include <sys/select.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
 // DEFINE
 
 #define HELLO "HELLO WORLD!\n"
@@ -69,6 +71,13 @@ int	print_err(char *str, int socket_fd, client *array_of_client)
 	return (1);
 }
 
+int	get_nbr_char(size_t	nbr)
+{
+	if (nbr < 10)
+		return (1);
+	return (get_nbr_char(nbr / 10) + 1);
+}
+
 int	main(int argc, char **argv)
 {
 	int	retval;
@@ -98,29 +107,19 @@ int	main(int argc, char **argv)
 	address.sin_addr.s_addr = inet_addr("127.0.0.1");// pas autorise
 	address.sin_port = htons(get_port(argv[1]));
 
-#include <stdio.h>
 	if (bind(socketfd, (const struct sockaddr *)(&address), sizeof(address)) < 0)
-	{
-		perror("bind");
-		my_print("ici", 1);
 		return (print_err(ERR_PARAM_SERV, 2, array_of_client) , 1);
-	}
 	if (listen(socketfd, SOMAXCONN) < 0)
-	{
-		my_print("la", 1);
 		return (print_err(ERR_PARAM_SERV, 2, array_of_client) , 1);
-	}
 
 	int	taller_fd = socketfd;
 	fd_set	rfds;
-
+	char	*msg_new_c = "serveur : le client %ld vient d'arriver\n";
 
 	while (1)
 	{
 		FD_ZERO(&rfds);
 		FD_SET(socketfd, &rfds);
-		// FD_SET(0, &rfds);
-		retval = 0;
 		retval = select(taller_fd + 1, &rfds, NULL, NULL, NULL);
 
 		/* Don't rely on the value of tv now! */
@@ -137,9 +136,48 @@ int	main(int argc, char **argv)
 				client_fd = accept(socketfd, (struct sockaddr *)&client_addr, &client_addr_len);
 				if (client_fd < 0)
 					return (print_err(ERR_PARAM_SERV, 2, array_of_client) , 1);
-				printf("Un nouveau client est connecté ! FD = %d\n", client_fd);
-				// Tu peux ajouter ce fd à un tableau de clients ici, si tu veux suivre plusieurs
+				size_t	i = 0;
+				while (i < SOMAXCONN && array_of_client[i].libre == 0)
+				{
+					i++;
+				}
+				if (array_of_client[i].libre == 0)
+					return (print_err(ERR_OTHER, 2, array_of_client) , 1);
+
+				array_of_client[i].libre = 0;
+				array_of_client[i].id = n_ac;
+				array_of_client[i].client_fd = client_fd;
+				array_of_client[i].client_addr = client_addr;
+				array_of_client[i].client_addr_len = sizeof(array_of_client[i].client_addr);
+				n_ac++;
+
+				size_t	new_id = array_of_client[i].id;
+				char	msg[strlen(msg_new_c) + get_nbr_char(array_of_client[i].id) - 2];
+				i = 0;
+				sprintf(msg, msg_new_c, new_id);
+				while (i < SOMAXCONN)
+				{
+					if (array_of_client[i].libre == 0)
+						send(array_of_client[i].client_fd, msg, strlen(msg_new_c) + get_nbr_char(new_id) - 2, 0);
+					i++;
+				}
 			}
+			// else
+			// {
+			// 	size_t i = 0;
+			// 	int	find = 1;
+			// 	while (find && i < SOMAXCONN)
+			// 	{
+			// 		if (array_of_client[i].libre == 0 && (FD_ISSET(array_of_client[i].client_fd, &rfds)))
+			// 			find = 0;
+			// 		else
+			// 			i++;
+			// 	}
+			// 	if (find)
+			// 		return (print_err(ERR_OTHER, 2, array_of_client) , 1);
+			// 	client	c = array_of_client[i];
+
+			// }
 			// Savoirs qui est 
 		}
 		// ajouter
